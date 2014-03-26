127a74741a3248e8f21423b5df4e83d3dfab76e6
Krzysztof Kula <krzychukula@gmail.com>
2014-03-26 14:45:15 +0100

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Krzysztof Kula on 26.03.2014.
+//  Copyright (c) 2014 krzychukula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
